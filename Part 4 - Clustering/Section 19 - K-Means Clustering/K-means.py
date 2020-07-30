949f37e6f59b73dc5531c4ecc10da157c302d486
kanzakiken <wtkanzakiken@gmail.com>
2020-07-30 19:57:09 +0900

    -

---
+++ Part 4 - Clustering/Section 19 - K-Means Clustering/K-means.py
@@ -0,0 +1,25 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, 3:5].values
+
+# Using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, max_iter=300, n_init=10, init='k-means++', random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+# Applying the k-means to the mall dataset
+kmeans = KMeans(n_clusters=5, max_iter=300, n_init=10, init='k-means++', random_state=0)
--- Part 4 - Clustering/Section 19 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 19 - K-Means Clustering/kmeans.py
@@ -12,15 +12,16 @@ X = dataset.iloc[:, 3:5].values
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
-for i in range(1,11):
-    kmeans = KMeans(n_clusters = i, max_iter = 300, n_init = 10, init = 'k-means++', random_state = 0)
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, max_iter=300, n_init=10, init='k-means++', random_state=0)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
-plt.plot(range(1,11), wcss)
+plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of Clusters')
 plt.ylabel('WCSS')
 plt.show()
+
 
 # Applying the k-means to the mall dataset
 kmeans = KMeans(n_clusters = 5, max_iter = 300, n_init = 10, init = 'k-means++', random_state = 0)
@@ -38,4 +39,3 @@ plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
 plt.show()
-
